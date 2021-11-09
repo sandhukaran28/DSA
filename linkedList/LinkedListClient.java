cfccd669c01057ee782be938e70b950f2d0d7625
sandhukaran28 <60167401+sandhukaran28@users.noreply.github.com>
2021-11-09 06:20:12 +0530

    linked list added

--- linkedList/LinkedListClient.java
+++ linkedList/LinkedListClient.java
@@ -7,18 +7,23 @@ public class LinkedListClient {
 		MyLinkedList ll = new MyLinkedList();
 
 		ll.addAtFirst(10);
-		ll.addAtFirst(20);
-		ll.addAtLast(50);
-		ll.display();
-		ll.AddAtIndex(100, 1);
-		ll.display();
-
-		System.out.println(ll.size());
-
-		System.out.println(ll.getFirst());
-
-		System.out.println(ll.getLast());
-		System.out.println(ll.getAtIndex(0));
+//		ll.addAtFirst(20);
+//		ll.addAtLast(50);
+//		ll.display();
+//		ll.AddAtIndex(100, 1);
+//		ll.display();
+//		ll.addAtLast(110);
+//		ll.display();
+		
+		System.out.println(ll.middle());
+//		ll.reverse();
+//		ll.display();
+//		System.out.println(ll.size());
+//
+//		System.out.println(ll.getFirst());
+//
+//		System.out.println(ll.getLast());
+//		System.out.println(ll.getAtIndex(0));
 
 	}
 
--- linkedList/MyLinkedList.java
+++ linkedList/MyLinkedList.java
@@ -80,7 +80,7 @@ public class MyLinkedList {
 
 		while (i != null) {
 
-			System.out.print(i.val + " ");
+			System.out.print(i.val + "->");
 			i = i.next;
 		}
 		System.out.println();
@@ -139,5 +139,92 @@ public class MyLinkedList {
 
 		return i;
 	}
+
+	public int deleteFirst() throws Exception {
+		if (head == null) {
+			throw new Exception("LinkedList Empty");
+		}
+		Node i = head;
+		head = head.next;
+		i.next = null;
+		int temp = i.val;
+		i = null;
+		return temp;
+
+	}
+	
+	public int deleteLast() throws Exception {
+		
+		if(size() == 1) {
+			return deleteFirst();
+		}
+		Node i = getNodeAtIndex(size()-2);
+		int temp = getNodeAtIndex(size()-1).val;
+		i.next = null;
+		return temp;
+	}
+	
+	public int deleteAIndex(int idx) throws Exception {
+		
+		if(idx == 0) {
+			return deleteFirst();
+		}
+		else if(idx == size()-1) {
+			return deleteLast();
+		}
+		else {
+			
+			Node pre = getNodeAtIndex(idx-1);
+			Node curr = pre.next;
+			Node nx = pre.next.next;
+			
+			pre.next = nx;
+			curr.next = null;
+			int temp = curr.val;
+			curr = null;
+			return temp;
+		}
+		
+	}
+	
+	public void reverse() {
+		
+		if(head == null || head.next == null) {	
+			return;
+		}
+		
+		Node pre = null;
+		Node curr = head;
+		
+		while(curr!=null) {
+			Node nx= curr.next;
+			curr.next = pre;
+			pre = curr;
+			curr = nx;
+		}
+		head = pre;
+	}
+	
+	public int middle() {
+		
+		Node slow = head;
+		Node fast = head;
+		
+		while(fast!=null && fast.next !=null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		
+		return slow.val;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
