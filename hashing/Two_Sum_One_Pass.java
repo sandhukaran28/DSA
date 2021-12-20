3b82373763f225575f216fc9a400fa5555779eae
sandhukaran28 <60167401+sandhukaran28@users.noreply.github.com>
2021-12-20 15:39:46 +0530

    new questions added

--- hashing/Two_Sum_One_Pass.java
+++ hashing/Two_Sum_One_Pass.java
@@ -1,5 +1,28 @@
 package hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Two_Sum_One_Pass {
+
+	public int[] twoSum(int[] nums, int target) {
+		int n = nums.length;
+		Map<Integer, Integer> hs = new HashMap<>();
+		int arr[] = new int[2];
+		for (int i = 0; i < n; i++) {
+
+			if (hs.containsKey(target - nums[i])) {
+
+				arr[0] = i;
+				arr[1] = hs.get(target - nums[i]);
+				return arr;
+
+			}
+
+			hs.put(nums[i], i);
+		}
+
+		return arr;
+	}
 
 }
